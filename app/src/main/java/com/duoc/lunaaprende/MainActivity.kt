d61d3a932deafaf043cd599d94b2bd9eb17c0f6b
naomiigit <nao.villarroel@duocuc.cl>
2025-10-14 22:53:22 -0300

    aaa

--- app/src/main/java/com/duoc/lunaaprende/MainActivity.kt
+++ app/src/main/java/com/duoc/lunaaprende/MainActivity.kt
@@ -4,17 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import com.duoc.lunaaprende.ui.Navegacion
 import com.duoc.lunaaprende.ui.theme.LunaAprendeTheme
-import com.duoc.lunaaprende.ui.theme.PaginaUno
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LunaAprendeTheme {
-                PaginaUno(navController = NavController)
+                Navegacion()
             }
         }
     }
